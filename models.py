c070f49baeb256a4e89146cc1e5eee2dee0ca26e
rhema254 <karhem254@gmail.com>
2024-09-25 14:57:55 +0300

    update database references

--- models.py
+++ models.py
@@ -43,7 +43,7 @@ class users(db.Model):
     
 
 
-class PaymentPlan(db.Model):
+class paymentplans(db.Model):
     __tablename__ = 'payment_plans'
     id = db.Column(db.Integer, primary_key=True)
     plan_name = db.Column(db.String(100), nullable=False)
@@ -66,8 +66,8 @@ class PaymentPlan(db.Model):
             setattr(self, key, value)
         self.save()
 
-class FlexiblePlan(db.Model):
-    __tablename__ = 'flexible_plans'
+class flexibleplans(db.Model):
+    __tablename__ = 'flexibleplans'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     total_amount = db.Column(db.Integer, nullable=False)
@@ -76,10 +76,10 @@ class FlexiblePlan(db.Model):
     duration_months = db.Column(db.Integer, nullable=False)  # User-defined duration
     created_at = db.Column(db.DateTime, default=db.func.now())
     
-    user = db.relationship('User', back_populates='flexible_plans')
+    user = db.relationship('users', back_populates='flexible_plans')
 
 
-class Payment(db.Model):
+class payments(db.Model):
     __tablename__ = 'payments'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -103,8 +103,8 @@ class Payment(db.Model):
         self.save()
 
 
-class InstallmentSchedule(db.Model):
-    __tablename__ = 'installment_schedules'
+class installmentschedules(db.Model):
+    __tablename__ = 'installmentschedules'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     plan_id = db.Column(db.Integer, db.ForeignKey('payment_plans.id'), nullable=False)
@@ -126,7 +126,7 @@ class InstallmentSchedule(db.Model):
             setattr(self, key, value)
         self.save()
 
-class Complaint(db.Model):
+class complaints(db.Model):
     __tablename__ = 'complaints'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -148,8 +148,8 @@ class Complaint(db.Model):
             setattr(self, key, value)
         self.save()
 
-class TransactionHistory(db.Model):
-    __tablename__ = 'transaction_history'
+class transactionhistory(db.Model):
+    __tablename__ = 'transactionhistory'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     payment_id = db.Column(db.Integer, db.ForeignKey('payments.id'), nullable=False)
